b44ed632e53541c06cbe14c6ab4d286127d93d7c
late <late@LATE-PC>
2017-02-08 20:52:02 +0200

    mission 5

---
+++ RadioApplication/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio();
+            radio.Power = false;           
+            int choice = 0;
+            while (true)
+            {
+                Console.WriteLine("Welcome to Late's radio program. What would you like to do today?");
+                if (radio.Power == false)
+                {
+                    Console.WriteLine("1. Turn on the radio");
+                }
+                else
+                {
+                    Console.WriteLine("1. Turn off the radio");
+                }
+                Console.WriteLine("2. Set the volume level");
+                Console.WriteLine("3. Change the channel");
+                Console.WriteLine("4. Check radio status");
+                string line = Console.ReadLine();
+                choice = int.Parse(line);
+                if (choice == 1)
+                {
+                    if (radio.Power == false)
+                    {
+                        radio.Power = true;
+                        radio.Volume = 2;
+                        radio.Channel = 2000;
+                    }
+                    else if (radio.Power == true)
+                    {
+                        radio.Power = false;
+                    }
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine("Please set the new volume level:");
+                    string line1 = Console.ReadLine();
+                    radio.Volume = int.Parse(line1);
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("Please set the new channel frequency (2000 - 20000):");
+                    string line2 = Console.ReadLine();
+                    radio.Channel = int.Parse(line2);
+                }
+                else if (choice == 4)
+                {
+                    radio.RadStatus();
+                }
+                else
+                    Console.WriteLine("Not a valid number.");
+
+
+            }
+        }
+    }
+}
---
+++ RadioApplication/Radio.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioApplication
+{
+    class Radio
+    {
+        private readonly int maxVol = 9;
+        private readonly int minVol = 0;
+        private int volume;
+        private readonly double minChan = 2000.00;
+        private readonly double maxChan = 20000.00;
+        private double chan;
+        private bool power;
+        public void RadStatus()
+        {
+            if (Power == false)
+            {
+                Console.WriteLine("The radio is powered off.");
+            }
+            else
+            {
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Radio Status: ");
+                Console.WriteLine("Volume: " + Volume);
+                Console.WriteLine("Current Channel: " + Channel);
+                Console.WriteLine("-----------------------------------");
+            }
+        }
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (Power == false)
+                {
+                    Console.WriteLine("The radio is currenty powered off. You may want to consider turning it on first.");
+                }
+                else
+                { 
+                    if (value >= 0 && value <= 9)
+                    {
+                        volume = value;
+                    }
+                    else if (value < 0)
+                    {
+                        Console.WriteLine("Volume can't be lower than zero, you dummy.");
+                        Console.WriteLine("We'll just set it to zero for now.");
+                        volume = minVol;
+                    }
+                    else if (value > 9)
+                    {
+                        Console.WriteLine("Volume can't be higher than 9");
+                        Console.WriteLine("We'll just set it to the max for now.");
+                        volume = maxVol;
+                    }
+                }
+            }
+        }
+                 
+        public bool Power { get; set; }
+           
+        public double Channel {
+            get
+            {
+                return chan;
+            }
+                
+            set
+            {
+                if (Power == false)
+                {
+                    Console.WriteLine("The radio is currenty powered off. You may want to consider turning it on first.");
+                }
+                else
+                { 
+                    if (value >= 2000 && value <= 20000)
+                    {
+                        chan = value;
+                    }
+                    else if (value < 2000)
+                    {
+                        Console.WriteLine("Unfortunately, 2000 is the lowest we can go.");
+                        Console.WriteLine("As punishment for your mistake, we are defaulting you to the worst station.");
+                        Channel = 6969;
+                    }
+                    else if (value > 20000)
+                    {
+                        Console.WriteLine("Unfortunately, 20000 is the highest we can go.");
+                        Console.WriteLine("As punishment for your mistake, we are defaulting you to the worst station.");
+                        Channel = 6969;
+                    }
+                }
+            }
+         }
+
+    }
+}
